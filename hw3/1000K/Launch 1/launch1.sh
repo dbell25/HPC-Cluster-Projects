516c3cb2a754513b1a9efcd44c4da10992d808d9
Daniel Bell <djbell216@gmail.com>
2018-10-04 10:31:39 -0500

    Round 4 Data - Complete

---
+++ hw3/1000K/Launch 1/launch1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+##$ -l h_rt=0:02:00
+
+for j in 1 2 3 4 5
+  do
+     sbatch --constraint=elves --time=480 --mem-per-cpu=2048 --partition=ksu-gen-reserved.q,batch.q --ntasks-per-node=1 --nodes=$j run.sh 
+done
