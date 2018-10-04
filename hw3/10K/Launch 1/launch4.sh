478f650093595cfa6f67230b9fda3c10ff1e57a5
Daniel Bell <djbell216@gmail.com>
2018-10-04 13:39:02 -0500

    Data Collection Complete

--- hw3/10K/Launch 1/launch4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-##$ -l h_rt=0:02:00
-
-for j in 1 2 3 4 5
-  do
-     sbatch --constraint=elves --partition=ksu-gen-reserved.q,batch.q --ntasks-per-node=8 --nodes=$j run.sh
-done
-
--- hw3/10K/Launch 1/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -l
-module load OpenMPI
-mpicc -Wall -Wextra -O0 find_keys.c -o find_keys
-mpirun /homes/danieljbell/cis625/hw3/find_keys
